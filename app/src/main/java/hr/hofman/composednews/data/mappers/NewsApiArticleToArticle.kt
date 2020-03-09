9bfcf3538d0bd3cd6c5c519466aa467b8ea2fef9
Tomislav Hofman <tomislav.hofman@gmail.com>
2020-03-09 09:33:00 +0100

    Refactor Article -> Headline

--- app/src/main/java/hr/hofman/composednews/data/mappers/NewsApiArticleToArticle.kt
+++ app/src/main/java/hr/hofman/composednews/data/mappers/NewsApiArticleToArticle.kt
@@ -1,6 +1,6 @@
 package hr.hofman.composednews.data.mappers
 
-import hr.hofman.composednews.data.local.Article
+import hr.hofman.composednews.data.local.Headline
 import hr.hofman.composednews.data.remote.NewsApiArticle
 import org.threeten.bp.Instant
 import javax.inject.Inject
@@ -8,10 +8,10 @@ import javax.inject.Singleton
 
 @Singleton
 class NewsApiArticleToArticle @Inject constructor(private val newsApiSourceToSource: NewsApiArticleSourceToSource) :
-    Mapper<NewsApiArticle, Article> {
+    Mapper<NewsApiArticle, Headline> {
 
-    override fun map(from: NewsApiArticle): Article =
-        Article(
+    override fun map(from: NewsApiArticle): Headline =
+        Headline(
             source = newsApiSourceToSource.map(from.source),
             author = from.author ?: "",
             title = from.title ?: "",
