6cff5e198de2058b917937f697cb54731ab9f4a4
Tomislav Hofman <tomislav.hofman@gmail.com>
2020-04-13 13:58:29 +0200

    Revert "Ci setup"

--- app/src/main/java/hr/hofman/composednews/ui/home/HomeFragment.kt
+++ app/src/main/java/hr/hofman/composednews/ui/home/HomeFragment.kt
@@ -1,16 +1,10 @@
 package hr.hofman.composednews.ui.home
 
-import android.os.Bundle
-import android.view.View
 import hr.hofman.composednews.base.FragmentBase
 
 class HomeFragment : FragmentBase() {
 
     override fun invalidate() {
         // TODO()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
     }
 }
